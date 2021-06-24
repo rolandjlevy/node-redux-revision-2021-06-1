6e8d8dd226357e265891a6b40229ee0749bba9af
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-06-24 22:59:11 +0000

    Another test

--- redux/reducer/index.test.js
+++ redux/reducer/index.test.js
@@ -4,11 +4,16 @@ const { INC_COUNTER, DEC_COUNTER } = require('../action-types');
 const action = { type: INC_COUNTER }
 const initialState = { counter: 0 };
 
-test('Reducer returns updated state', () => {
-  let result = reducer({ counter: 0 }, { type: INC_COUNTER });
-  expect(result.counter).toBe(1);
-  result = reducer({ counter: 5 }, { type: DEC_COUNTER });
-  expect(result.counter).toBe(4);
-  result = reducer({ counter: 0 }, { type: null });
-  expect(result.counter).toBe(0);
+test('should return various states', () => {
+  const inc = reducer({ counter: 0 }, { type: INC_COUNTER });
+  expect(inc.counter).toBe(1);
+  const dec = reducer({ counter: 5 }, { type: DEC_COUNTER });
+  expect(dec.counter).toBe(4);
+  const def = reducer({ counter: 0 }, { type: null });
+  expect(def.counter).toBe(0);
 });
+
+test('should return the initial state', () => {
+  let result = reducer(undefined, {});
+  expect(result).toEqual({ counter: 0 });
+})
